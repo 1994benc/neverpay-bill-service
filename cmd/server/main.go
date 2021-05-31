50228c8c55506da033cc10c616ec8908f8796f8e
Ben <ben.chomsang@cedus.co.uk>
2021-05-31 08:51:06 +0000

    Structure the entry file for the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+)
 
+type App struct{}
+
+// Run - runs our application. We set it up in a struct so that it's easy for testing
+func (app *App) Run() error {
+	log.Println("Running the server")
+	return nil
+}
+
+func main() {
+	app := App{}
+	err := app.Run()
+	if err != nil {
+		log.Fatalf("Error starting the server %s", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
 module 1994benc/neverpay-api
 
 go 1.16
-
-require (
-	github.com/golang/mock v1.5.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/gorilla/mux v1.8.0
-)
